d0c527bb64e39c1ea40eb5d59a9cd8f1052257e7
Daniel <71837281+darmiel@users.noreply.github.com>
2022-05-24 09:20:23 +0200

    feat: use axios

--- index.js
+++ index.js
@@ -1,41 +1,44 @@
 const core = require('@actions/core');
+const axios = require('axios').default;
 
-try {
-    // get url, name and secret from core
-    let url = core.getInput('url');
-    // check if protocol is set in url
-    if (!url.startsWith('http')) {
-        url = 'https://' + url;
-    }
-    // check if url ends with / and remove it
-    if (url.endsWith('/')) {
-        url = url.substring(0, url.length - 1);
-    }
-
-    const name = core.getInput('name');
-    const secret = core.getInput('secret');
-
-    // make get request to target
-    const target = `${url}/${encodeURI(name)}`;
-    console.log("requesting", target, "...");
-
-    // send request to target using XHR
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', target, true);
-    xhr.setRequestHeader('X-YADWH-Secret', secret);
-    xhr.onload = function () {
-        core.setOutput('response', xhr.responseText);
-        if (xhr.status !== 200) {
-            console.log("error:", xhr.statusText);
-            core.setFailed(xhr.statusText);
+async function run() {
+    try {
+        // get url, name and secret from core
+        let url = core.getInput('url');
+        // check if protocol is set in url
+        if (!url.startsWith('http')) {
+            url = 'https://' + url;
         }
-    };
-    xhr.onerror = function () {
-        console.log("error:", xhr.statusText);
-        core.setFailed(xhr.statusText);
-    };
-    xhr.send();
+        // check if url ends with / and remove it
+        if (url.endsWith('/')) {
+            url = url.substring(0, url.length - 1);
+        }
+
+        const name = core.getInput('name');
+        const secret = core.getInput('secret');
+
+        // make get request to target
+        const target = `${url}/${encodeURI(name)}`;
+        console.log("requesting", target, "...");
+
+        // send request to target using XHR
+        axios.get(target, {
+            headers: {
+                'X-YADWH-Secret': secret
+            }
+        }).then(function (response) {
+            console.log("response received");
+            console.log(response.data);
+            core.setOutput(response.data);
+        }).catch(function (error) {
+            console.log("error received");
+            console.log(error);
+            core.setFailed(error);
+        });
+    }
+    catch (error) {
+        core.setFailed(error.message);
+    }
 }
-catch (error) {
-    core.setFailed(error.message);
-}
+
+run()
