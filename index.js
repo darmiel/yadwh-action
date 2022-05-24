6edb4de2f21570208118dca0e1b355336d674c0b
Daniel <71837281+darmiel@users.noreply.github.com>
2022-05-24 09:02:27 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const core = require('@actions/core');
+
+try {
+    // get url, name and secret from core
+    let url = core.getInput('url');
+    // check if protocol is set in url
+    if (!url.startsWith('http')) {
+        url = 'https://' + url;
+    }
+    // check if url ends with / and remove it
+    if (url.endsWith('/')) {
+        url = url.substring(0, url.length - 1);
+    }
+
+    const name = core.getInput('name');
+    const secret = core.getInput('secret');
+
+    // make get request to target
+    const target = `${url}/${encodeURI(name)}`;
+    console.log("requesting", target, "...");
+
+    (async () => {
+        const response = await fetch(target, {
+            method: 'GET',
+            headers: {
+                'X-YADWH-Secret': secret
+            }
+        });
+
+        // get response body
+        const body = await response.text();
+
+        // output response body
+        core.setOutput('response', body);
+    })();
+} catch (error) {
+    core.setFailed(error.message);
+}
